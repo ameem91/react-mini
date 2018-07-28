d24ac14692a4d22775bcb60e2f46ad3e178fd6ff
Ameem Shaik <ashaik@ebsco.com>
2018-07-28 16:36:53 -0400

    remove redundant webpack setting

--- webpack.demo.config.js
+++ webpack.demo.config.js
@@ -3,7 +3,6 @@ const HtmlWebpackPlugin = require("html-webpack-plugin");
 module.exports = {
     mode: "development",
     entry: "./src/demo",
-    devtool: false,
     plugins: [
         new HtmlWebpackPlugin({
             title: "react-mini demo"
